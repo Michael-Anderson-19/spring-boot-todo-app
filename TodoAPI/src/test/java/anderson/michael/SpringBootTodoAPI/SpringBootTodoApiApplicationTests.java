6955e56e92a7ee1cb4b8544e9e3ac9b4618668be
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2022-04-15 02:32:56 +0100

    Initial commit
    Initial commit of the base spring boot project

---
+++ TodoAPI/src/test/java/anderson/michael/SpringBootTodoAPI/SpringBootTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package anderson.michael.SpringBootTodoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
